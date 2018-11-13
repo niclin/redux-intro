6e3fe03c22e6df8b5c8735caaaa4a74ad0ac4516
niclin <bboyceo@hotmail.com>
2018-11-13 23:50:32 +0800

    Add connect

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+function mapStateToProps(state) {
+  return {
+    count: state.count
+  };
+}
 
 class App extends Component {
   state = { count: 0 }
 
   increment = () => {
-    this.setState({
-      count: this.state.count + 1
-    });
+
   }
 
   decrement = () => {
-    this.setState({
-      count: this.state.count - 1
-    });
+
   }
 
   render() {
@@ -29,4 +32,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(mapStateToProps)(App);
